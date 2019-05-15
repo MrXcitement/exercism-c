6f2c7fa772b8488a682a41c6bff67cbb0e5df7d1
Mike Barker <mike@thebarkers.com>
2019-05-15 20:50:36 +0000

    Changes based on mentor comments

--- c/isogram/src/isogram.c
+++ c/isogram/src/isogram.c
@@ -1,6 +1,7 @@
-/* isogram.c --- Determine if a word or phrase is an isogram.
- *  Mike Barker <mike@thebarkers.com>
- *  May 4th, 2019
+/*
+ * isogram.c --- Determine if a word or phrase has no duplicate letters, an isogram.
+ * Mike Barker
+ * May 4th, 2019
  */
 
 #include <ctype.h>
@@ -12,18 +13,17 @@ bool is_isogram(const char *s) {
 	if ( s == NULL ) return false;
 
 	bool seen_letter[26] = { false };
-	int letter_idx = 0;	
+	int letter_idx = 0;
 
 	for ( int i = 0; i < (int)strlen( s ); i++ ) {
 		if ( ! isalpha( s[i] )) {
 			continue;
 		}
-		letter_idx = (int)tolower( s[i] ) - (int)'a';
+		letter_idx = tolower( s[i] ) - 'a';
 		if ( seen_letter[letter_idx] ) {
-			return false;
+			return true;
 		}
 		seen_letter[letter_idx] = true;
 	}
 	return true;
 }
-
